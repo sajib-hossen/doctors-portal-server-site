4735de8952082302695245aaec61f32814078b4a
sajib hossen <sajibhossensa90@gmail.com>
2022-04-09 02:11:56 +0600

    first comment

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+// dotenv file
+
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+const uri = ` mongodb+srv://${process.env.S3_BUCKET}:${process.env.SECRET_KEY}@cluster0.tvvz8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+  } finally {
+    //   await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
